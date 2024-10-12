783fd3e834769863740f1e4fb8be46e6560a6809
NurlChl <nurul.cholil2373@gmail.com>
2024-10-13 00:09:51 +0700

    verify

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -464,56 +464,41 @@
                         </li>
                     </form>
                 @else
-                    <div class="absolute bottom-0 left-0">
+                    <!-- Dropdown menu -->
+                    <ul class="text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownLogReg">
+                        <li>
+                            <a href="{{ route('login') }}"
+                                class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group {{ request()->routeIs('login') ? 'bg-gray-100' : '' }}">
 
-                        <button id="dropdownLogReg" data-dropdown-toggle="dropdownLoginRegis" data-dropdown-placement="top"
-                            class=" me-2 mb-2 md:mb-0 text-gray-800 rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center"
-                            type="button"><svg class="w-2 h-2 text-gray-800" aria-hidden="true"
-                                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                    stroke-width="2" d="M9 5 5 1 1 5" />
-                            </svg>
-                        </button>
+                                <svg class="flex-shrink-0 w-6 h-6 text-gray-400  transition duration-75  group-hover:text-gray-900   {{ request()->routeIs('login*') ? 'text-gray-900' : '' }}"
+                                    aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                    fill="none" viewBox="0 0 24 24">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                        stroke-width="1"
+                                        d="M16 12H4m12 0-4 4m4-4-4-4m3-4h2a3 3 0 0 1 3 3v10a3 3 0 0 1-3 3h-2" />
+                                </svg>
 
-                        <!-- Dropdown menu -->
-                        <div id="dropdownLoginRegis"
-                            class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
-                            <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownLogReg">
-                                <li>
-                                    <a href="{{ route('login') }}"
-                                        class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group {{ request()->routeIs('login') ? 'bg-gray-100' : '' }}">
-        
-                                        <svg class="flex-shrink-0 w-6 h-6 text-gray-400  transition duration-75  group-hover:text-gray-900   {{ request()->routeIs('login*') ? 'text-gray-900' : '' }}"
-                                            aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                            fill="none" viewBox="0 0 24 24">
-                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                                stroke-width="1"
-                                                d="M16 12H4m12 0-4 4m4-4-4-4m3-4h2a3 3 0 0 1 3 3v10a3 3 0 0 1-3 3h-2" />
-                                        </svg>
-        
-        
-                                        <span class="ml-3">LOGIN</span>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="{{ route('register') }}"
-                                        class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group {{ request()->routeIs('register') ? 'bg-gray-100' : '' }}">
-        
-                                        <svg class="flex-shrink-0 w-6 h-6 text-gray-400  transition duration-75  group-hover:text-gray-900 }  {{ request()->routeIs('register*') ? 'text-gray-900' : '' }}"
-                                            aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                            fill="none" viewBox="0 0 24 24">
-                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                                stroke-width="1"
-                                                d="M18 5V4a1 1 0 0 0-1-1H8.914a1 1 0 0 0-.707.293L4.293 7.207A1 1 0 0 0 4 7.914V20a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-5M9 3v4a1 1 0 0 1-1 1H4m11.383.772 2.745 2.746m1.215-3.906a2.089 2.089 0 0 1 0 2.953l-6.65 6.646L9 17.95l.739-3.692 6.646-6.646a2.087 2.087 0 0 1 2.958 0Z" />
-                                        </svg>
-        
-        
-                                        <span class="ml-3">REGISTER</span>
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
+
+                                <span class="ml-3">LOGIN</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="{{ route('register') }}"
+                                class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group {{ request()->routeIs('register') ? 'bg-gray-100' : '' }}">
+
+                                <svg class="flex-shrink-0 w-6 h-6 text-gray-400  transition duration-75  group-hover:text-gray-900 }  {{ request()->routeIs('register*') ? 'text-gray-900' : '' }}"
+                                    aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                    fill="none" viewBox="0 0 24 24">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                        stroke-width="1"
+                                        d="M18 5V4a1 1 0 0 0-1-1H8.914a1 1 0 0 0-.707.293L4.293 7.207A1 1 0 0 0 4 7.914V20a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-5M9 3v4a1 1 0 0 1-1 1H4m11.383.772 2.745 2.746m1.215-3.906a2.089 2.089 0 0 1 0 2.953l-6.65 6.646L9 17.95l.739-3.692 6.646-6.646a2.087 2.087 0 0 1 2.958 0Z" />
+                                </svg>
+
+
+                                <span class="ml-3">REGISTER</span>
+                            </a>
+                        </li>
+                    </ul>
                 @endauth
 
 
