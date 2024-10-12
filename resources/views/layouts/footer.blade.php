c49295f52b829f558c079cc0296999880874d588
NurlChl <nurul.cholil2373@gmail.com>
2024-10-12 13:28:18 +0700

    npm run build

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -5,52 +5,54 @@
                 <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">UKM</h2>
                 <ul class="text-gray-500 dark:text-gray-400 font-medium grid grid-cols-2 gap-x-8">
                     <li class="mb-4">
-                        <a href="#" class=" hover:underline">Simfoni FM</a>
+                        <a href="https://www.instagram.com/simfonifmmalang/" class=" hover:underline">Simfoni FM</a>
                     </li>
                     <li class="mb-4">
-                        <a href="#" class="hover:underline">Jhepret Club Fotografi</a>
+                        <a href="https://www.instagram.com/jhepretclubfotografi/" class="hover:underline">Jhepret Club
+                            Fotografi</a>
                     </li>
                     <li class="mb-4">
-                        <a href="#" class="hover:underline">LKP2M</a>
+                        <a href="https://www.instagram.com/lkp2m_uinmlg/" class="hover:underline">LKP2M</a>
                     </li>
                     <li class="mb-4">
-                        <a href="#" class="hover:underline">USPM Inovasi</a>
+                        <a href="https://www.instagram.com/uapminovasi/" class="hover:underline">LPM UAP Minovasi
+                        </a>
                     </li>
                     <li class="mb-4">
-                        <a href="#" class="hover:underline">KSR PMI</a>
+                        <a href="ksrpmiunituinmalang" class="hover:underline">KSR PMI</a>
                     </li>
                     <li class="mb-4">
-                        <a href="#" class="hover:underline">Teater K2</a>
+                        <a href="https://www.instagram.com/teaterk2/" class="hover:underline">Teater Komedi Kontemporer</a>
                     </li>
                     <li class="mb-4">
-                        <a href="#" class="hover:underline">Unior</a>
+                        <a href="https://www.instagram.com/unior_uinmalang/" class="hover:underline">Unit Olahraga UIN Malang</a>
                     </li>
                     <li class="mb-4">
                         <a href="#" class="hover:underline">Pramuka</a>
                     </li>
                     <li class="mb-4">
-                        <a href="#" class="hover:underline">Seni Religius</a>
+                        <a href="https://www.instagram.com/pramukauinmlg/" class="hover:underline">Seni Religius</a>
                     </li>
                     <li class="mb-4">
-                        <a href="#" class="hover:underline">Taekwondo Indonesia</a>
+                        <a href="https://www.instagram.com/taekwondo_uinmalang/" class="hover:underline">Taekwondo</a>
                     </li>
                     <li class="mb-4">
-                        <a href="#" class="hover:underline">KOPMA Padang Bulan</a>
+                        <a href="https://www.instagram.com/kopma_padang_bulan/" class="hover:underline">KOPMA Padang Bulan</a>
                     </li>
                     <li class="mb-4">
-                        <a href="#" class="hover:underline">KOMMUST</a>
+                        <a href="https://www.instagram.com/ukmkommust/" class="hover:underline">KOMMUST</a>
                     </li>
                     <li class="mb-4">
-                        <a href="#" class="hover:underline">Satuan Resimen Mahasiswa</a>
+                        <a href="https://www.instagram.com/menwa811_uinmalang/" class="hover:underline">Resimen Mahasiswa</a>
                     </li>
                     <li class="mb-4">
-                        <a href="#" class="hover:underline">IPS NU Pagar Nusa</a>
+                        <a href="https://www.instagram.com/senireligius/" class="hover:underline">Seni Religius</a>
                     </li>
                     <li class="mb-4">
-                        <a href="#" class="hover:underline">MAPALASKA</a>
+                        <a href="https://www.instagram.com/mapalatursina/" class="hover:underline">Mapala Tursina</a>
                     </li>
                     <li class="mb-4">
-                        <a href="#" class="hover:underline">PSM Gema Gita Buana</a>
+                        <a href="https://www.instagram.com/psmggb.uinmalang/" class="hover:underline">PSM Gema Gita Buana</a>
                     </li>
                 </ul>
             </div>
@@ -58,25 +60,25 @@
                 <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Dema F</h2>
                 <ul class="text-gray-500 dark:text-gray-400 font-medium">
                     <li class="mb-4">
-                        <a href="#" class="hover:underline">Dema Fak. Syariah</a>
+                        <a href="#" class="hover:underline">DEMA Fak. Syariah</a>
                     </li>
                     <li class="mb-4">
-                        <a href="#" class="hover:underline">Dema Fak. FITK</a>
+                        <a href="#" class="hover:underline">DEMA Fak. FITK</a>
                     </li>
                     <li class="mb-4">
-                        <a href="#" class="hover:underline">Dema Fak. Humaniora</a>
+                        <a href="#" class="hover:underline">DEMA Fak. Humaniora</a>
                     </li>
                     <li class="mb-4">
-                        <a href="#" class="hover:underline">Dema Fak. Ekonomi</a>
+                        <a href="#" class="hover:underline">DEMA Fak. Ekonomi</a>
                     </li>
                     <li class="mb-4">
-                        <a href="#" class="hover:underline">Dema Fak. Saintek</a>
+                        <a href="#" class="hover:underline">DEMA Fak. Saintek</a>
                     </li>
                     <li class="mb-4">
-                        <a href="#" class="hover:underline">Dema Fak. Psikologi</a>
+                        <a href="#" class="hover:underline">DEMA Fak. Psikologi</a>
                     </li>
                     <li class="mb-4">
-                        <a href="#" class="hover:underline">Dema Fak. FKIK</a>
+                        <a href="#" class="hover:underline">DEMA Fak. FKIK</a>
                     </li>
                 </ul>
             </div>
@@ -84,25 +86,25 @@
                 <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Sema F</h2>
                 <ul class="text-gray-500 dark:text-gray-400 font-medium">
                     <li class="mb-4">
-                        <a href="#" class="hover:underline">Sema Fak. Syariah</a>
+                        <a href="#" class="hover:underline">SEMA Fak. Syariah</a>
                     </li>
                     <li class="mb-4">
-                        <a href="#" class="hover:underline">Sema Fak. FITK</a>
+                        <a href="#" class="hover:underline">SEMA Fak. FITK</a>
                     </li>
                     <li class="mb-4">
-                        <a href="#" class="hover:underline">Sema Fak. Humaniora</a>
+                        <a href="#" class="hover:underline">SEMA Fak. Humaniora</a>
                     </li>
                     <li class="mb-4">
-                        <a href="#" class="hover:underline">Sema Fak. Ekonomi</a>
+                        <a href="#" class="hover:underline">SEMA Fak. Ekonomi</a>
                     </li>
                     <li class="mb-4">
-                        <a href="#" class="hover:underline">Sema Fak. Saintek</a>
+                        <a href="#" class="hover:underline">SEMA Fak. Saintek</a>
                     </li>
                     <li class="mb-4">
-                        <a href="#" class="hover:underline">Sema Fak. Psikologi</a>
+                        <a href="#" class="hover:underline">SEMA Fak. Psikologi</a>
                     </li>
                     <li class="mb-4">
-                        <a href="#" class="hover:underline">Sema Fak. FKIK</a>
+                        <a href="#" class="hover:underline">SEMA Fak. FKIK</a>
                     </li>
                 </ul>
             </div>
@@ -110,17 +112,18 @@
                 <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Universitas</h2>
                 <ul class="text-gray-500 dark:text-gray-400 font-medium">
                     <li class="mb-4">
-                        <a href="#" class=" hover:underline">Sema U</a>
+                        <a href="#" class=" hover:underline">SEMA U</a>
                     </li>
                     <li class="mb-4">
-                        <a href="#" class="hover:underline">Dema U</a>
+                        <a href="#" class="hover:underline">DEMA U</a>
                     </li>
                 </ul>
             </div>
-            
+
             <div class="lg:hidden flex flex-col gap-2 justify-end md:items-end">
                 <h2 class="text-xl sm:text-3xl font-bold text-start md:text-end uppercase">RM UIN Maliki</h2>
-                <p class="text-gray-500 text-start md:text-end text-xs">Developed by <span class="text-black">NurlChl</span> & <span class="text-black">Fitriasnt</span></p>
+                <p class="text-gray-500 text-start md:text-end text-xs">Developed by <span
+                        class="text-black">NurlChl</span> & <span class="text-black">Fitriasnt</span></p>
             </div>
         </div>
         <div class="px-4 py-6 bg-gray-800 md:flex md:items-center md:justify-between">
